a49b1ac9f324ea0c74cb6e482994a59d898d58b9
r1chwo0n <woonzy09@gmail.com>
2023-07-10 16:08:56 +0700

    finish

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
+const firstNameInput = document.querySelector("#first-name-input");
+const lastNameInput = document.querySelector("#last-name-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const submitBtn = document.querySelector("#submit-btn");
+
 //simple email validation
 function validateEmail(email) {
   var atPos = email.indexOf("@");
   var dotPos = email.lastIndexOf(".");
   return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
 }
+
+firstNameInput.onkeyup = () => {
+  firstNameInput.classList.remove("is-valid");
+  firstNameInput.classList.remove("is-invalid");
+};
+
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+};
+
+emailInput.onkeyup = () => {
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+};
+
+passwordInput.onkeyup = () => {
+  passwordInput.classList.remove("is-valid");
+  passwordInput.classList.remove("is-invalid");
+};
+
+submitBtn.onclick = () => {
+  let isfirstnameOk = false;
+  let islastnameOk = false;
+  let isemailOk = false;
+  let ispasswordOk = false;
+  if (firstNameInput.value === "") {
+    firstNameInput.classList.add("is-invalid");
+  } else {
+    firstNameInput.classList.add("is-valid");
+    isfirstnameOk = true;
+  }
+
+  if (lastNameInput.value === "") {
+    lastNameInput.classList.add("is-invalid");
+  } else {
+    lastNameInput.classList.add("is-valid");
+    islastnameOk = true;
+  }
+
+  if (validateEmail(emailInput.value)) {
+    emailInput.classList.add("is-valid");
+    isemailOk = true;
+  } else {
+    emailInput.classList.add("is-invalid");
+  }
+
+  if (passwordInput.value.length >= 6) {
+    passwordInput.classList.add("is-valid");
+    ispasswordOk = true;
+  } else {
+    passwordInput.classList.add("is-invalid");
+  }
+
+  if (isfirstnameOk & islastnameOk & isemailOk & ispasswordOk) {
+    alert("Registed successfully");
+  }
+};
